5c016d8346e297ad6f18f89df151d70b3f926beb
Siddharth Subramanian <sidsbrmnn@gmail.com>
2022-03-23 22:40:53 +0530

    fix candidate elimination

--- candidate_elimination.py
+++ candidate_elimination.py
@@ -6,7 +6,7 @@ concepts = np.array(data.iloc[:, :-1])
 target = np.array(data.iloc[:, -1])
 
 specific_h = concepts[0].copy()
-general_h = [["?" for _ in range(len(specific_h))] for _ in range(len(concepts))]
+general_h = [["?" for _ in range(len(specific_h))] for _ in range(len(specific_h))]
 
 for i, h in enumerate(concepts):
     if target[i] == "Yes":
@@ -24,8 +24,7 @@ for i, h in enumerate(concepts):
 indices = [
     i for i, h in enumerate(general_h) if h == ["?" for _ in range(len(specific_h))]
 ]
-for i in indices:
-    general_h.pop(i)
+general_h = np.delete(general_h, indices, axis=0)
 
 print("Final S:", specific_h)
 print("Final G:", general_h)
